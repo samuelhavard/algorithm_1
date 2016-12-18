40fb3d88f759c4c13ab9b12f4e47d4a3bab9cd86
Samuel Havard <SamuelHavard@yahoo.com>
2016-12-18 09:22:37 -0500

    update

--- alg.py
+++ alg.py
@@ -1,8 +1,25 @@
-x = 5
-y = 3
-z = 0
+# x = 5
+# y = 3
+# z = 0
+#
+# while x > 0:
+#     z += y
+#     x -= 1
+# print z
 
-while x > 0:
-    z += y
-    x -= 1
-print z
+
+# def clique(n):
+#     count = 0
+#     print "in a clique..."
+#     count += 1
+#     for j in range(n):
+#         for i in range(j):
+#             count += 1
+#     print count
+# clique(4)
+
+
+def clique_check(n):
+    return ((n * (n-1))/2) + (2 + n)
+
+print clique_check(4)
